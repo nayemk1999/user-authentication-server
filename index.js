e7a756a9087bc9e868e971da15fb30ea284078c6
Nayem Khan <nayemk900@gmail.com>
2021-09-11 02:38:39 +0600

    api setup done

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+// MONGO_URL = mongodb+srv://userData:user1234@cluster0.dwwpp.mongodb.net/userData?retryWrites=true&w=majority
+dotenv.config();
+const port = process.env.PORT || 5050;
+const mongoURL = process.env.MONGO_URL;
+
+mongoose
+  .connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    
+  })
+  .then(() => console.log("Database connected!"))
+  .catch((err) => console.log(err));
+
+// middleware
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/user", userRoute);
+app.use("/auth", authRoute);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log("Listening");
+});
